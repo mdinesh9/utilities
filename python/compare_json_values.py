c2723ec61350627b9bf07068f24ba215bebfe5f5
Dinesh <dineshm.csit@gmail.com>
2016-04-09 21:19:48 +0530

    Python files moved to python folder

--- python/compare_json_values.py
+++ python/compare_json_values.py
@@ -1,8 +1,10 @@
 # Compares two json files and prints the differences in the values.
-# The keys were in ascending order in the two json files used here.
+# The keys were in ascending order in the two json files.
 
 
 import json
+
+l = 30000 # change l value when required.
 
 with open("with_strip.json") as json_file:
     with_strip_data = json.load(json_file)
@@ -10,7 +12,7 @@ with open("with_strip.json") as json_file:
 with open("without_strip.json") as json_file:
     without_strip_data = json.load(json_file)
 
-for count in range(2,36779):
+for count in range(2,l):
     if with_strip_data[str(count)] != without_strip_data[str(count)]:
         print count,':',with_strip_data[str(count)].encode('utf-8'),',',without_strip_data[str(count)].encode('utf-8')
 
